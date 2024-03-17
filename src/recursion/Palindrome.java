5d66fc2c7d14bbded471ce540a0b2efab6ef3bab
Prakash Raut <prakash_raut@outlook.com>
2024-03-17 11:19:37 +0530

    Added Palindrome.java

---
+++ src/recursion/Palindrome.java
@@ -0,0 +1,22 @@
+package recursion;
+
+public class Palindrome {
+    public static boolean isPalindrome(String s) {
+        return isPalindromeHelper(s, 0, s.length() - 1);
+    }
+
+    private static boolean isPalindromeHelper(String s, int l, int r) {
+        // even: l > r  | odd : l == r
+        if (l >= r) return true;
+        // check for extreme match
+        if (s.charAt(l) == s.charAt(r)) {
+            return isPalindromeHelper(s, l + 1, r - 1);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String s = "racecar";
+        System.out.println("isPalindrome(s) = " + isPalindrome(s));
+    }
+}
