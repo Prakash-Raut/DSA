273d91b8d10a2a9aebefe13256d895098f1223a0
Prakash Raut <prakash_raut@outlook.com>
2024-03-18 23:37:32 +0530

    Added fastPowerWithModulo

--- src/recursion/PowerFunction.java
+++ src/recursion/PowerFunction.java
@@ -13,8 +13,21 @@ public class PowerFunction {
         return a * fastPower(a, b - 1);
     }
 
+    public static double fastPowerWithModulo(double a, int b, int m) {
+        if (b < 0) {
+            a = (1 / a);
+            b = -b;
+        }
+        if (b == 0) return 1;
+        if (b % 2 == 0) return fastPowerWithModulo((a * a) % m, b / 2, m);
+        return (a * fastPowerWithModulo(a, b - 1, m) % m) % m;
+    }
+
+
     public static void main(String[] args) {
         System.out.println("power(5, 4) = " + power(5, 4));
         System.out.println("fastPower(5, 4) = " + fastPower(5, 4));
+        int m = (int) (1e9 + 7);
+        System.out.println("fastPowerWithModulo(5,4,m) = " + fastPowerWithModulo(5, 4, m));
     }
 }
